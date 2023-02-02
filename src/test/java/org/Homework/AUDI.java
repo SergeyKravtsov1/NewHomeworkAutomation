0d0f6d16603f8a07a8c4b255ebc37ddc6178c067
Serg <serhiikravtsovkharkiv@ukr.net>
2023-02-02 12:27:44 +0200

    Апдейт по замечаниям. 1)Вроде как заполнил поля(в обсуждению к домашке скинул скрин с уточнением эти ли поля) 2)Заполнил интерфейс 3)Сделал в car модификаторы доступа 4)В Enam сделал в верхнем регистре

--- src/test/java/org/Homework/AUDI.java
+++ src/test/java/org/Homework/AUDI.java
@@ -1,13 +1,8 @@
 package org.Homework;
 
-public class AUDI extends Car {
+public class AUDI implements Test {
 
-    private int years;
-    private String type;
-    public AUDI(String color, int speed, double price) {
-        super(color, speed, price);
-
-
+    public void drive() {
 
     }
-}
+    }
---
+++ src/test/java/org/Homework/Test.java
@@ -0,0 +1,5 @@
+package org.Homework;
+
+public interface Test {
+    void drive();
+}
